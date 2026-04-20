e6d3af17aebfd26c3916509c7234a87269f8fcc1
Laura Ratcliff <laura_ratcliff@hotmail.com>
2026-04-20 16:14:57 +0100

    conflicting change

--- pixel_art.py
+++ pixel_art.py
@@ -5,24 +5,16 @@ def define_bee():
     image_mat = np.full((16, 16, 3), 1.0)
 
     # define some colours
-    black = [0, 0, 0]
+    black = [0.5, 0, 0]
     blue = [0.0, 0.4, 1.0]
     grey = [0.65] * 3
 
     # specify which pixels are which colour
-<<<<<<< master
     image_mat[7:11, 2] = yellow
     image_mat[6:12, 3:5] = yellow
     image_mat[6:12, 5:7] = yellow
     image_mat[6:12, 7:9] = yellow
     image_mat[6:12, 9:11] = yellow
-=======
-    image_mat[7:11, 2] = black
-    image_mat[6:12, 3:5] = black
-    image_mat[6:12, 5:7] = blue
-    image_mat[6:12, 7:9] = black
-    image_mat[6:12, 9:11] = blue
->>>>>>> master
     image_mat[6:12, 11:13] = black
     image_mat[7:11, 13] = yellow
     image_mat[4:6, 5:11] = grey
